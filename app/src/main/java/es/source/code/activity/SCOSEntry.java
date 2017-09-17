dcf2e3b0ee396f58e7125e8e2bfcb9720fd9cd3c
Daniel-panmengze <452705789@qq.com>
2017-09-17 14:49:47 +0800

    初次提交。

---
+++ app/src/main/java/es/source/code/activity/SCOSEntry.java
@@ -0,0 +1,14 @@
+package es.source.code.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import es.source.code.R;
+
+public class SCOSEntry extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entry);
+    }
+}
